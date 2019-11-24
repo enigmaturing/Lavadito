6476715b6301e8f6cf3874ce4ee927e419e2e18e
Javier Gonzalez <javier.glez.martin@gmail.com>
2019-11-24 17:12:34 +0100

    Change icon of the app to the new one
    Drag and drop all images from Android Asset Studio to the Resources folder, start project, stop it, go to properties of the android project: Android Manifest -> Application Icon -> Select the new icon -> Save -> Run application again (the new icon should be now shown for the app)

--- Lavadito/Lavadito.Android/MainActivity.cs
+++ Lavadito/Lavadito.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Lavadito.Droid
 {
-    [Activity(Label = "Lavadito", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Lavadito", Icon = "@mipmap/ic_launcher_new", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
