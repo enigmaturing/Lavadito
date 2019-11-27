e44cf1b118d00c7febf5e87fb77d8bee17f1e9dc
Javier Gonzalez <javier.glez.martin@gmail.com>
2019-11-27 11:41:10 +0100

    Display messagebox if no token is provided, to avoid crashes

--- Lavadito/Lavadito/MainPage.xaml.cs
+++ Lavadito/Lavadito/MainPage.xaml.cs
@@ -31,12 +31,19 @@ namespace Lavadito
 
         private async void toggleMotor(string s)
         {
-            using (var httpClient = new HttpClient())
+            if (tokenEntry.Text == null)
             {
-                var formcontent = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("arg", s) });
-
-                var request = await httpClient.PostAsync("https://api.particle.io/v1/devices/e00fce68ba9a1f5ea4870186/motorToggle?access_token=" + tokenEntry.Text.ToString(), formcontent);
+                await DisplayAlert("Alert", "Insert a valid token first!", "OK");
             }
+            else
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var formcontent = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("arg", s) });
+                    var request = await httpClient.PostAsync("https://api.particle.io/v1/devices/e00fce68ba9a1f5ea4870186/motorToggle?access_token=" + tokenEntry.Text.ToString(), formcontent);
+                }
+            }
+
         }
     }
 }
